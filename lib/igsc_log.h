05f1d2055f753aaba0551579736aa26dac2102ab
Solaris17 <solaris17@outlook.com>
2024-11-06 21:58:07 -0600

    IGSC changes

--- lib/igsc_log.h
+++ lib/igsc_log.h
@@ -53,37 +53,31 @@
 const char *gsc_time(char *buffer, size_t buff_len);
 #define gsc_debug(_fmt_, ...) \
     if (igsc_get_log_level() >= IGSC_LOG_LEVEL_DEBUG) { \
-        char __time_buf[MAX_TIME_STRING_LEN]; \
         if (NULL == igsc_get_log_callback_func()) { \
-            debug_print("%s: " PACKAGE_LOG_NAME ": (%s:%s():%d) " _fmt_, \
-                        gsc_time(__time_buf, sizeof(__time_buf)), __FILE__, __func__, __LINE__,  ##__VA_ARGS__); \
+            debug_print(_fmt_, ##__VA_ARGS__); \
         } else { \
-            igsc_log_func_t igsc_log_func = igsc_get_log_callback_func(); igsc_log_func(IGSC_LOG_LEVEL_DEBUG, "%s: " PACKAGE_LOG_NAME ": (%s:%s():%d) " _fmt_, \
-                        gsc_time(__time_buf, sizeof(__time_buf)), __FILE__, __func__, __LINE__,  ##__VA_ARGS__); \
-        }   \
+            igsc_log_func_t igsc_log_func = igsc_get_log_callback_func(); \
+            igsc_log_func(IGSC_LOG_LEVEL_DEBUG, _fmt_, ##__VA_ARGS__); \
+        } \
     }
 
 #define gsc_trace(_fmt_, ...) \
     if (igsc_get_log_level() >= IGSC_LOG_LEVEL_TRACE) { \
-        char __time_buf[MAX_TIME_STRING_LEN]; \
         if (NULL == igsc_get_log_callback_func()) { \
-            trace_print("%s: " PACKAGE_LOG_NAME ": (%s:%s():%d) " _fmt_, \
-                        gsc_time(__time_buf, sizeof(__time_buf)), __FILE__, __func__, __LINE__,  ##__VA_ARGS__) \
+            trace_print(_fmt_, ##__VA_ARGS__); \
         } else { \
-              igsc_log_func_t igsc_log_func = igsc_get_log_callback_func(); igsc_log_func(IGSC_LOG_LEVEL_TRACE, "%s: " PACKAGE_LOG_NAME ": (%s:%s():%d) " _fmt_, \
-                        gsc_time(__time_buf, sizeof(__time_buf)), __FILE__, __func__, __LINE__,  ##__VA_ARGS__); \
+            igsc_log_func_t igsc_log_func = igsc_get_log_callback_func(); \
+            igsc_log_func(IGSC_LOG_LEVEL_TRACE, _fmt_, ##__VA_ARGS__); \
         } \
     }
 
 #define gsc_error(_fmt_, ...) \
     if (NULL == igsc_get_log_callback_func()) { \
-        char __time_buf[MAX_TIME_STRING_LEN]; \
-        error_print("%s: " PACKAGE_LOG_NAME ": (%s:%s():%d) " _fmt_, \
-                    gsc_time(__time_buf, sizeof(__time_buf)), __FILE__, __func__, __LINE__, ##__VA_ARGS__); \
+        error_print(_fmt_, ##__VA_ARGS__); \
     } else { \
-        char __time_buf[MAX_TIME_STRING_LEN]; \
-        igsc_log_func_t igsc_log_func = igsc_get_log_callback_func(); igsc_log_func(IGSC_LOG_LEVEL_ERROR, "%s: " PACKAGE_LOG_NAME ": (%s:%s():%d) " _fmt_, \
-                    gsc_time(__time_buf, sizeof(__time_buf)), __FILE__, __func__, __LINE__,  ##__VA_ARGS__); \
+        igsc_log_func_t igsc_log_func = igsc_get_log_callback_func(); \
+        igsc_log_func(IGSC_LOG_LEVEL_ERROR, _fmt_, ##__VA_ARGS__); \
     }
+
 
 #endif /* __IGSC_UTILS_H__ */
